c29c56cb6667ed7201838d0fb90a3e643ee818f8
arkteck <arkteck@gmail.com>
2024-07-14 11:43:59 -0400

    implement MealItem"

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final meals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
     Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (ctx) => const MealsScreen(
-            title: 'Some title',
-            meals: [],
+          builder: (ctx) => MealsScreen(
+            title: category.title,
+            meals: meals,
           ),
         ));
   }
@@ -35,7 +39,7 @@ class CategoriesScreen extends StatelessWidget {
             .map((category) => CategoryGridItem(
                   category: category,
                   onSelectCategory: () {
-                    _selectCategory(context);
+                    _selectCategory(context, category);
                   },
                 ))
             .toList(),
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -15,7 +16,7 @@ class MealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget body = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (ctx, i) => Text(meals[i].title),
+      itemBuilder: (ctx, i) => MealItem(meal: meals[i]),
     );
     if (meals.isEmpty) {
       body = Center(
