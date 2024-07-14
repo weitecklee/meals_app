d56abca4b6970cd2d4aaa9469c0c0d502a7dc276
arkteck <arkteck@gmail.com>
2024-07-14 11:17:52 -0400

    use Navigator.push to move to screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (ctx) => const MealsScreen(
+            title: 'Some title',
+            meals: [],
+          ),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +32,12 @@ class CategoriesScreen extends StatelessWidget {
         ),
         padding: const EdgeInsets.all(24),
         children: availableCategories
-            .map((category) => CategoryGridItem(category: category))
+            .map((category) => CategoryGridItem(
+                  category: category,
+                  onSelectCategory: () {
+                    _selectCategory(context);
+                  },
+                ))
             .toList(),
       ),
     );
