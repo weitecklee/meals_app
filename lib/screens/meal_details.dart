297ca0ddfcc8fa3861be700e50a09b76d3aee10e
arkteck <arkteck@gmail.com>
2024-07-15 11:19:56 -0400

    refactor, favorite icon reflects status

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -12,6 +12,10 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
           title: Text(
@@ -35,7 +39,7 @@ class MealDetailsScreen extends ConsumerWidget {
                   ),
                 );
               },
-              icon: const Icon(Icons.star),
+              icon: Icon(isFavorite ? Icons.star : Icons.star_border),
             ),
           ]),
       body: SingleChildScrollView(
